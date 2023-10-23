d99b5a6d94f84b61385ca46f75416a4681e88cec
Iulian Ichim <ichim.iulian12@gmail.com>
2023-10-24 01:48:55 +0300

    completely changed UI layout and strategy. Modified config settings.

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from shiny import App, Inputs, Outputs, Session, render
 from shiny import experimental as x
 from shiny import reactive, ui
 
-from modules.common import show_table, show_graph
+from config import Config
+
+from modules.common_ui import show_table, show_graph
 
 from modules.summary.ui import summary_inputs
 from modules.summary.server import (update_filename_input, load_data_frame, update_aggregator_input,
@@ -10,46 +12,57 @@ from modules.summary.server import (update_filename_input, load_data_frame, upda
 
 from modules.distributions.ui import distribution_selection
 from modules.distributions.server import (create_distribution_inputs, load_distribution_data,
-                                          update_distribution_inputs, create_distribution_data_set)
+                                          update_distribution_inputs, create_distribution_data_set, distribution_graph)
+
+app_width = Config.ui_config('width')
+app_height = Config.ui_config('height')
 
 # column choices
 app_ui = x.ui.page_fillable(
-    ui.layout_sidebar(
-        ui.panel_sidebar(
-            {"class": "p-3"},
-            ui.navset_tab_card(
-                ui.nav("Data Summarizer", summary_inputs('summary')),
-                ui.nav('Distributions', distribution_selection('distributions'), ui.output_ui('distribution_inputs')),
-            ),
-        ),
-        x.ui.panel_main(
-            x.ui.layout_column_wrap(
-                1,
-                ui.output_ui('_table'),
+    ui.navset_tab_card(
+        ui.nav(
+            "Data Summarizer",
+            x.ui.layout_sidebar(
+                x.ui.sidebar(
+                    {"class": "p-3"},
+                    summary_inputs('summary'),
+                    width=app_width
+                ),
                 x.ui.layout_column_wrap(
                     1,
-                    ui.output_ui('_graph')
+                    show_table('summary'),
+                    x.ui.layout_column_wrap(
+                        1,
+                        show_graph('summary')
+                    ),
                 ),
-            ),
-
-            # x.ui.layout_column_wrap(
-            #     1,
-            #     show_table('summary'),
-            #     x.ui.layout_column_wrap(
-            #         1,
-            #         show_graph('summary')
-            #     ),
-            # )
-        )
-
-    ),
+                height=app_height
+            )
+        ),
+        ui.nav(
+            'Distributions',
+            x.ui.layout_sidebar(
+                x.ui.sidebar(
+                    {"class": "p-3"},
+                    distribution_selection('distributions'), ui.output_ui('distribution_inputs'),
+                    width=app_width
+                ),
+                x.ui.layout_column_wrap(
+                    1,
+                    show_table('distributions'),
+                    show_graph('distributions')
+                ),
+                height=app_height
+            )
+        ),
+    )
 )
 
 
 def server(input: Inputs, output: Outputs, session: Session):
     original_df = reactive.Value()
     grouper = reactive.Value()
-    data_frame = reactive.Value()
+    summary_df = reactive.Value()
     distribution_df = reactive.Value()
 
     update_filename_input('summary')
@@ -60,19 +73,9 @@ def server(input: Inputs, output: Outputs, session: Session):
 
     update_graph_input('summary', grouper)
 
-    load_summary_data('summary', original_df, data_frame)
+    load_summary_data('summary', original_df, summary_df)
 
-    create_graph('summary', filter_df('summary', original_df, data_frame))
-
-    @output
-    @render.ui
-    def _table():
-        return show_table('summary')
-
-    @output
-    @render.ui
-    def _graph():
-        return show_graph('summary')
+    create_graph('summary', filter_df('summary', original_df, summary_df))
 
     @output
     @render.ui
@@ -85,5 +88,7 @@ def server(input: Inputs, output: Outputs, session: Session):
 
     load_distribution_data('distributions', distribution_df)
 
+    distribution_graph('distributions', distribution_df)
 
-app = App(app_ui, server, debug=True)
+
+app = App(app_ui, server)
