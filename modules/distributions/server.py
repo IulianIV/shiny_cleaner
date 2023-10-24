b431cb8ab9ca61cef56222c65a46d430ceea3502
Iulian Ichim <ichim.iulian12@gmail.com>
2023-10-25 01:48:00 +0300

    Added Exponential distribution. Keeping DRY.

--- modules/distributions/server.py
+++ modules/distributions/server.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 import numpy as np
+import pandas
 import pandas as pd
+import shiny.reactive
 
 from shiny import Inputs, Outputs, Session, module, render, ui, reactive
 from shinywidgets import render_widget
@@ -9,11 +11,12 @@ import shiny.experimental as x
 import plotly.express as px
 import plotly.graph_objs as go
 
-from utils import synchronize_size, two_dim_to_one_dim
+from utils import synchronize_size, two_dim_to_one_dim, create_distribution_df
 
 from config import Config
 
 graph_height = Config.ui_config('graph_height')
+distribution_types = Config.server_config('distribution_types')
 
 
 @module.server
@@ -27,6 +30,7 @@ def create_distribution_inputs(input: Inputs, output: Outputs, session: Session)
         sd = Config.input_config('distributions_standard_deviation_sigma')
         mean = Config.input_config('distributions_mean_mu')
         events = Config.input_config('distributions_events')
+        scale = Config.input_config('distributions_scale')
 
         if input.distributions() == 'Gaussian':
             return (
@@ -52,6 +56,23 @@ def create_distribution_inputs(input: Inputs, output: Outputs, session: Session)
                     ui.column(3, ui.input_numeric('min', 'Min', value=min_val)),
                     ui.column(3, ui.input_numeric('max', 'Max', value=max_val)),
                     ui.column(3, ui.input_numeric('events', 'Events', value=events)),
+                ), x.ui.tooltip(
+                    ui.input_switch('matrix', 'Matrix'),
+                    # TODO find a way to show this as `(input.min(), input.max())` with actual values
+                    "(Min, Max) matrix of values",
+                    id="matrix_tip", placement='left'
+                ),
+                ui.input_slider('observations', 'Observations', min=min_val, max=max_val,
+                                value=max_val / 2),
+                ui.input_action_button('plot_distribution', 'Plot')
+            )
+
+        if input.distributions() == 'Exponential':
+            return (
+                ui.row(
+                    ui.column(3, ui.input_numeric('min', 'Min', value=min_val)),
+                    ui.column(3, ui.input_numeric('max', 'Max', value=max_val)),
+                    ui.column(3, ui.input_numeric('scale', 'Scale', value=scale)),
                 ), x.ui.tooltip(
                     ui.input_switch('matrix', 'Matrix'),
                     # TODO find a way to show this as `(input.min(), input.max())` with actual values
@@ -105,28 +126,29 @@ def create_distribution_data_set(input: Inputs, output: Outputs, session: Sessio
             sd = input.sd()
             mean = input.mean()
 
-            distribution = np.random.normal(mean, sd, obs)
-            distribution_df = pd.DataFrame(data=distribution, index=range(1, len(distribution) + 1),
-                                           columns=['value'])
-
-            if input.matrix():
-                distribution = np.random.normal(mean, sd, size=(input.min(), input.max()))
-                distribution_df = pd.DataFrame(data=distribution[:, :], index=range(1, len(distribution) + 1),
-                                               columns=[f'values_{x}' for x in range(1, distribution.shape[1] + 1)])
+            distribution_df = create_distribution_df('normal',
+                                                     {'mean': mean, 'sd': sd, 'obs': obs, 'min': input.min,
+                                                      'max': input.max},
+                                                     input.matrix)
 
             data_frame.set(distribution_df)
 
         if input.distributions() == 'Poisson':
             events = input.events()
 
-            distribution = np.random.poisson(events, obs)
-            distribution_df = pd.DataFrame(data=distribution, index=range(1, len(distribution) + 1),
-                                           columns=['value'])
+            distribution_df = create_distribution_df('poisson',
+                                                     {'events': events, 'obs': obs, 'min': input.min, 'max': input.max},
+                                                     input.matrix)
 
-            if input.matrix():
-                distribution = np.random.poisson(events,(input.min(), input.max()))
-                distribution_df = pd.DataFrame(data=distribution[:, :], index=range(1, len(distribution) + 1),
-                                               columns=[f'values_{x}' for x in range(1, distribution.shape[1] + 1)])
+            data_frame.set(distribution_df)
+
+        if input.distributions() == 'Exponential':
+            scale = input.scale()
+
+            distribution_df = create_distribution_df('exponential',
+                                                     {'scale': scale, 'obs': obs, 'min': input.min,
+                                                      'max': input.max},
+                                                     input.matrix)
 
             data_frame.set(distribution_df)
 
@@ -153,45 +175,31 @@ def distribution_graph(input: Inputs, output: Outputs, session: Session, data_fr
         plot_data = data_frame()
         widget = None
 
-        if input.distributions() == 'Gaussian':
+        if input.matrix():
+            plot_data = two_dim_to_one_dim(plot_data, 'value')
 
-            if input.matrix():
-                plot_data = two_dim_to_one_dim(plot_data, 'value')
+        # Create the plot
+        hist = px.histogram(
+            plot_data,
+            x='value',
+            title=f'Histogram of {input.distributions()} distribution', height=graph_height)
 
-            # Create the plot
-            gauss_hist = px.histogram(
-                plot_data,
-                x='value',
-                title=f'Histogram of {input.distributions()} distribution', height=graph_height)
+        # TODO Add Kernel Density Estimation KDE or some sort of Probability Density Function
+        #   like the one from: https://numpy.org/doc/stable/reference/random/generated/numpy.random.normal.html
+        """
+        from scipy.stats import gaussian_kde
+        import numpy as np
+        array_plot_data = plot_data[plot_data.columns[0]].to_numpy()
+        kde = gaussian_kde(array_plot_data)
 
-            # TODO Add Kernel Density Estimation KDE or some sort of Probability Density Function
-            #   like the one from: https://numpy.org/doc/stable/reference/random/generated/numpy.random.normal.html
-            """
-            from scipy.stats import gaussian_kde
-            import numpy as np
-            array_plot_data = plot_data[plot_data.columns[0]].to_numpy()
-            kde = gaussian_kde(array_plot_data)
+        estimate1 = numpy.array([1 / (array_plot_data.std() * np.sqrt(2 * np.pi)) * np.exp(- (x -
+        array_plot_data.mean()) ** 2 / (2 * array_plot_data.std() ** 2)) for x in array_plot_data]) print(
+        plot_data) print(kde(array_plot_data)) kde_trace = go.Scatter(x=plot_data, y=kde(array_plot_data),
+        mode='lines', name='markers')
+        hist.add_trace(kde_trace)
+        """
 
-            estimate1 = numpy.array([1 / (array_plot_data.std() * np.sqrt(2 * np.pi)) * np.exp(- (x -
-            array_plot_data.mean()) ** 2 / (2 * array_plot_data.std() ** 2)) for x in array_plot_data]) print(
-            plot_data) print(kde(array_plot_data)) kde_trace = go.Scatter(x=plot_data, y=kde(array_plot_data),
-            mode='lines', name='markers')
-            hist.add_trace(kde_trace)
-            """
-
-            widget = go.FigureWidget(gauss_hist)
-
-        if input.distributions() == 'Poisson':
-
-            if input.matrix():
-                plot_data = two_dim_to_one_dim(plot_data, 'value')
-
-            poiss_hist = px.histogram(
-                plot_data,
-                x='value',
-                title=f'Histogram of {input.distributions()} distribution', height=graph_height)
-
-            widget = go.FigureWidget(poiss_hist)
+        widget = go.FigureWidget(hist)
 
         @synchronize_size("graph")
         def on_size_changed(width, height):
