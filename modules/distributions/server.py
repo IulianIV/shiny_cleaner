04ad6b4aae8b53ecb57e37adf6ecb3a90ea36ab8
Iulian Ichim <ichim.iulian12@gmail.com>
2023-10-25 21:05:19 +0300

    Fixed some bugs, added TODOs for other bugs, kept DRY

--- modules/distributions/server.py
+++ modules/distributions/server.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-
+import asyncio
 from shiny import Inputs, Outputs, Session, module, render, ui, reactive
 from shinywidgets import render_widget
 import shiny.experimental as x
@@ -12,7 +12,6 @@ from utils import synchronize_size, two_dim_to_one_dim, create_distribution_df
 from config import Config
 
 graph_height = Config.ui_config('graph_height')
-distribution_types = Config.server_config('distribution_types')
 
 
 @module.server
@@ -23,76 +22,75 @@ def create_distribution_inputs(input: Inputs, output: Outputs, session: Session)
     def inputs():
         min_val = Config.input_config('distributions_min')
         max_val = Config.input_config('distributions_max')
+
+        distributions_ui_defaults = {
+            'numerics': (ui.column(3, ui.input_numeric('min', 'Min', value=min_val)),
+                         ui.column(3, ui.input_numeric('max', 'Max', value=max_val))),
+            'switch': (x.ui.tooltip(
+                ui.input_switch('matrix', 'Matrix'),
+                # TODO find a way to show this as `(input.min(), input.max())` with actual values
+                "(Min, Max) matrix of values",
+                id="matrix_tip", placement='left'
+            )),
+            'slider': (ui.input_slider('observations', 'Observations', min=min_val, max=max_val,
+                                       value=max_val / 2)),
+            'plot_btn': (ui.input_action_button('plot_distribution', 'Plot'))
+        }
+
         sd = Config.input_config('distributions_standard_deviation_sigma')
         mean = Config.input_config('distributions_mean_mu')
         events = Config.input_config('distributions_events')
         scale = Config.input_config('distributions_scale')
         prob = Config.input_config('distributions_probability')
+        trials = Config.input_config('distributions_trials')
 
-        if input.distributions() == 'Gaussian':
+        if input.distributions() == 'Normal':
             return (
                 ui.row(
-                    ui.column(3, ui.input_numeric('min', 'Min', value=min_val)),
-                    ui.column(3, ui.input_numeric('max', 'Max', value=max_val)),
+                    distributions_ui_defaults['numerics'],
                     ui.column(3, ui.input_numeric('mean', 'μ', value=mean)),
                     ui.column(3, ui.input_numeric('sd', 'σ', value=sd))
-                ), x.ui.tooltip(
-                    ui.input_switch('matrix', 'Matrix'),
-                    # TODO find a way to show this as `(input.min(), input.max())` with actual values
-                    "(Min, Max) matrix of values",
-                    id="matrix_tip", placement='left'
-                ),
-                ui.input_slider('observations', 'Observations', min=min_val, max=max_val,
-                                value=max_val / 2),
-                ui.input_action_button('plot_distribution', 'Plot')
+                ), distributions_ui_defaults['switch'],
+                distributions_ui_defaults['slider'],
+                distributions_ui_defaults['plot_btn']
 
             )
         if input.distributions() == 'Poisson':
             return (
                 ui.row(
-                    ui.column(4, ui.input_numeric('min', 'Min', value=min_val)),
-                    ui.column(4, ui.input_numeric('max', 'Max', value=max_val)),
+                    distributions_ui_defaults['numerics'],
                     ui.column(4, ui.input_numeric('events', 'Events', value=events)),
-                ), x.ui.tooltip(
-                    ui.input_switch('matrix', 'Matrix'),
-                    "(Min, Max) matrix of values",
-                    id="matrix_tip", placement='left'
-                ),
-                ui.input_slider('observations', 'Observations', min=min_val, max=max_val,
-                                value=max_val / 2),
-                ui.input_action_button('plot_distribution', 'Plot')
+                ), distributions_ui_defaults['switch'],
+                distributions_ui_defaults['slider'],
+                distributions_ui_defaults['plot_btn']
             )
 
         if input.distributions() == 'Exponential':
             return (
-                ui.row(
-                    ui.column(4, ui.input_numeric('min', 'Min', value=min_val)),
-                    ui.column(4, ui.input_numeric('max', 'Max', value=max_val)),
-                    ui.column(4, ui.input_numeric('scale', 'Scale', value=scale)),
-                ), x.ui.tooltip(
-                    ui.input_switch('matrix', 'Matrix'),
-                    "(Min, Max) matrix of values",
-                    id="matrix_tip", placement='left'
-                ),
-                ui.input_slider('observations', 'Observations', min=min_val, max=max_val,
-                                value=max_val / 2),
-                ui.input_action_button('plot_distribution', 'Plot')
+                ui.row(distributions_ui_defaults['numerics'],
+                       ui.column(4, ui.input_numeric('scale', 'Scale', value=scale)),
+                       ), distributions_ui_defaults['switch'],
+                distributions_ui_defaults['slider'],
+                distributions_ui_defaults['plot_btn']
             )
 
         if input.distributions() == 'Geometric':
             return (
                 ui.row(
-                    ui.column(4, ui.input_numeric('min', 'Min', value=min_val)),
-                    ui.column(4, ui.input_numeric('max', 'Max', value=max_val)),
+                    distributions_ui_defaults['numerics'],
                     ui.column(4, ui.input_numeric('prob', 'Probability', value=prob)),
-                ), x.ui.tooltip(
-                    ui.input_switch('matrix', 'Matrix'),
-                    "(Min, Max) matrix of values",
-                    id="matrix_tip", placement='left'
-                ),
-                ui.input_slider('observations', 'Observations', min=min_val, max=max_val,
-                                value=max_val / 2),
-                ui.input_action_button('plot_distribution', 'Plot')
+                ), distributions_ui_defaults['switch'],
+                distributions_ui_defaults['slider'],
+                distributions_ui_defaults['plot_btn']
+            )
+        if input.distributions() == 'Binomial':
+            return (
+                ui.row(distributions_ui_defaults['numerics'],
+                       ui.column(4, ui.input_numeric('trials', 'Trials', value=trials)),
+                       ui.column(4, ui.input_numeric('prob', 'Probability', value=prob)),
+                       ), distributions_ui_defaults['switch'],
+                distributions_ui_defaults['slider'],
+                distributions_ui_defaults['plot_btn']
             )
 
 
@@ -110,25 +108,32 @@ def test_text(input: Inputs, output: Outputs, session: Session):
 """
 
 
+# TODO check #1 issue in GitHub. Apparently, having the `input.prob()` check in place, and inside the function, breaks
+#   functionality and the function fails is called on a distribution which does NOT generate the UI with that input.
+# TODO Also, even if the UI is generated, providing values >1 or <0 in the input, even with the functionality below to
+#   update it to permitted values, it first runs the DataFrame generation with the actual wrong values.
+#   This raises a ValueError
 @module.server
 def update_distribution_inputs(input: Inputs, output: Outputs, session: Session):
     @reactive.Effect
     @reactive.event(input.min, input.max, input.prob)
+    # all probability checks removed until #1 is fixed
+    # @reactive.event(input.min, input.max, input.prob)
     def update():
         current_value = input.observations()
-        min_val = input.min()
-        max_val = input.max()
-        prob_val = input.prob()
+        c_min_val = input.min()
+        c_max_val = input.max()
+        # prob_val = input.prob()
 
-        if min_val > max_val:
-            ui.update_numeric('min', value=max_val)
-            ui.update_numeric('max', value=min_val)
-            ui.update_slider('observations', min=max_val, max=min_val, value=current_value)
+        if c_min_val > c_max_val:
+            ui.update_numeric('min', value=c_max_val)
+            ui.update_numeric('max', value=c_min_val)
+            ui.update_slider('observations', min=c_max_val, max=c_min_val, value=current_value)
 
-        if prob_val > 1 or 0 > prob_val:
-            ui.update_numeric('prob', value=1)
+        # if prob_val > 1 or 0 > prob_val:
+        #     ui.update_numeric('prob', value=1)
 
-        ui.update_slider('observations', min=min_val, max=max_val, value=current_value)
+        ui.update_slider('observations', min=c_min_val, max=c_max_val, value=current_value)
 
 
 @module.server
@@ -137,11 +142,11 @@ def create_distribution_data_set(input: Inputs, output: Outputs, session: Sessio
     def data_set():
         obs = input.observations()
 
-        if input.distributions() == 'Gaussian':
+        if input.distributions() == 'Normal':
             sd = input.sd()
             mean = input.mean()
 
-            distribution_df = create_distribution_df('normal',
+            distribution_df = create_distribution_df(input.distributions().lower(),
                                                      {'mean': mean, 'sd': sd, 'obs': obs, 'min': input.min,
                                                       'max': input.max},
                                                      input.matrix)
@@ -151,7 +156,7 @@ def create_distribution_data_set(input: Inputs, output: Outputs, session: Sessio
         if input.distributions() == 'Poisson':
             events = input.events()
 
-            distribution_df = create_distribution_df('poisson',
+            distribution_df = create_distribution_df(input.distributions().lower(),
                                                      {'events': events, 'obs': obs, 'min': input.min, 'max': input.max},
                                                      input.matrix)
 
@@ -160,7 +165,7 @@ def create_distribution_data_set(input: Inputs, output: Outputs, session: Sessio
         if input.distributions() == 'Exponential':
             scale = input.scale()
 
-            distribution_df = create_distribution_df('exponential',
+            distribution_df = create_distribution_df(input.distributions().lower(),
                                                      {'scale': scale, 'obs': obs, 'min': input.min,
                                                       'max': input.max},
                                                      input.matrix)
@@ -170,13 +175,23 @@ def create_distribution_data_set(input: Inputs, output: Outputs, session: Sessio
         if input.distributions() == 'Geometric':
             prob = input.prob()
 
-            distribution_df = create_distribution_df('geometric',
+            distribution_df = create_distribution_df(input.distributions().lower(),
                                                      {'prob': prob, 'obs': obs, 'min': input.min,
                                                       'max': input.max},
                                                      input.matrix)
 
             data_frame.set(distribution_df)
 
+        if input.distributions() == 'Binomial':
+            prob = input.prob()
+            trials = input.trials()
+
+            distribution_df = create_distribution_df(input.distributions().lower(),
+                                                     {'trials': trials, 'prob': prob, 'obs': obs, 'min': input.min,
+                                                      'max': input.max},
+                                                     input.matrix)
+
+            data_frame.set(distribution_df)
 
 
 @module.server
