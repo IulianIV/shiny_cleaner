3af1a8e8219f5342ffe49236daa0541d06612503
Iulian Ichim <ichim.iulian12@gmail.com>
2023-10-25 21:04:36 +0300

    Added config constant

--- modules/common_ui.py
+++ modules/common_ui.py
@@ -2,6 +2,9 @@ from shiny import module, ui
 from shiny import experimental as x
 from shinywidgets import output_widget
 
+from config import Config
+
+graph_height_percent = Config.ui_config('graph_height_percent')
 
 @module.ui
 def show_table():
@@ -13,5 +16,5 @@ def show_table():
 @module.ui
 def show_graph():
     return x.ui.card(
-        output_widget('graph', height='100%')
+        output_widget('graph', height=graph_height_percent)
     )
