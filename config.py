f236ea84b1d0663e3ff2884e480a7db599494425
Iulian Ichim <ichim.iulian12@gmail.com>
2023-10-30 23:52:03 +0200

    Fixed order of some Distribution properties. Added Tooltip icon.

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-import math
+from shiny import ui
+
 
 class Config:
     __configs = ['ui', 'input']
@@ -12,8 +13,17 @@ class Config:
     __ui_config = {
         'width': 500,
         'height': 1200,
-        'graph_height': 550,
-        'graph_height_percent': '100%'
+        'graph_height': 500,
+        'graph_height_percent': '100%',
+        'tooltip_q': ui.HTML(
+            '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi '
+            'bi-question-circle-fill mb-1" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.496 '
+            '6.033h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 '
+            '.635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 '
+            '.25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 '
+            '0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286a.237.237 0 0 0 .241.247zm2.325 6.443c.61 0 '
+            '1.029-.394 1.029-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94 0 .533.425.927 1.01.927z"/></svg>'
+        )
     }
     __server_config = {}
     __input_config = {
@@ -23,13 +33,13 @@ class Config:
         },
         'distributions': {
             'continuous': {
-                'standard': ['Observations', 'CDF', 'PDF'],
+                'standard': ['Observations', 'PDF', 'CDF'],
                 'names': ['Uniform', 'Normal', 'Exponential'],
                 'methods': ['Log PDF', 'Log CDF', 'SF', 'Log SF'],
                 'extra_methods': ['PPF', 'ISF']
             },
             'discrete': {
-                'standard': ['Observations', 'CDF', 'PMF'],
+                'standard': ['Observations', 'PMF', 'CDF'],
                 'names': ['Binomial', 'Geometric', 'Poisson'],
                 'methods': ['Log PMF', 'Log CDF', 'SF', 'Log SF'],
                 'extra_methods': ['PPF', 'ISF']
@@ -38,7 +48,7 @@ class Config:
             },
             'mean': 1,
             'sd': 1.1,
-            'min_obs': 10,
+            'min_obs': 0,
             'max_obs': 100,
             'events': 5,
             'scale': 5,
