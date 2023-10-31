9b8c7a40652ab4e3c0944a64f74d57348803b452
Iulian Ichim <ichim.iulian12@gmail.com>
2023-10-31 15:53:52 +0200

    Moved main Distribution UI generating function from server side to ui side.

--- modules/distributions/server.py
+++ modules/distributions/server.py
@@ -8,7 +8,6 @@ import plotly.graph_objs as go
 from plotly.subplots import make_subplots
 
 from utils import synchronize_size, create_distribution_df
-from .ui import label_with_tooltip
 
 from config import Config
 
@@ -18,87 +17,6 @@ dist_defaults = config.input_config('distributions')
 
 cont_dist = dist_defaults['continuous']
 discrete_dist = dist_defaults['discrete']
-dist_names = cont_dist['names'] + discrete_dist['names']
-
-
-@module.server
-def create_dist_settings(input: Inputs, output: Outputs, session: Session):
-    @output
-    @render.ui
-    @reactive.event(input.distributions)
-    def inputs():
-        min_val = dist_defaults['min_obs']
-        max_val = dist_defaults['max_obs']
-        sd = dist_defaults['sd']
-        mean = dist_defaults['mean']
-        events = dist_defaults['events']
-        scale = dist_defaults['scale']
-        prob = dist_defaults['probability']
-        trials = dist_defaults['trials']
-        low = dist_defaults['low']
-        high = dist_defaults['high']
-
-        properties_tooltip_text = 'Extra properties to Table & Plot'
-        seed_tooltip = 'By setting a seed the data set can be frozen'
-
-        dist_inputs = None
-
-        if input.distributions() == 'Normal':
-            dist_inputs = (ui.column(3, ui.input_numeric('mean', 'μ', value=mean)),
-                           ui.column(3, ui.input_numeric('sd', 'σ', value=sd)))
-
-        elif input.distributions() == 'Poisson':
-            dist_inputs = ui.column(4, ui.input_numeric('events', 'Events', value=events))
-
-        elif input.distributions() == 'Exponential':
-            dist_inputs = ui.column(4, ui.input_numeric('scale', 'Scale', value=scale))
-
-        elif input.distributions() == 'Geometric':
-            dist_inputs = ui.column(4, ui.input_numeric('prob', 'Probability', value=prob, step=0.05))
-
-        elif input.distributions() == 'Binomial':
-            dist_inputs = (ui.column(3, ui.input_numeric('trials', 'Trials', value=trials)),
-                           ui.column(3, ui.input_numeric('prob', 'Probability', value=prob, step=0.05)))
-
-        elif input.distributions() == 'Uniform':
-            dist_inputs = (ui.column(3, ui.input_numeric('low', 'Low', value=low)),
-                           ui.column(3, ui.input_numeric('high', 'High', value=high)))
-
-        # elif input.distributions() == 'Cauchy':
-        #     dist_inputs = (ui.column(3, ui.input_numeric('scale', 'Scale', value=scale)),
-        #                    ui.column(3, ui.input_numeric('location', 'Location', value=0))
-        #                    )
-
-        dist_head = (ui.column(3, ui.input_numeric('seed', label_with_tooltip('Seed ', True, seed_tooltip, 'right',
-                                                                              'seed_tooltip'), value=0)),
-                     ui.column(3, ui.input_numeric('max', 'Max Obs.', value=max_val)))
-
-        dist_options = (ui.input_selectize('prop',
-                                           label_with_tooltip('Properties ', True, properties_tooltip_text, 'right',
-                                                              'card_tooltip'),
-                                           discrete_dist['methods'], multiple=False),
-                        ui.row(ui.column(6, ui.input_checkbox('enbl_extra', 'Extra Properties'))),
-                        ui.panel_conditional('input.enbl_extra',
-                                             ui.input_selectize('extra_prop', 'Extra Properties',
-                                                                discrete_dist['extra_methods'],
-                                                                multiple=False)),
-                        ui.input_slider('observations', 'Observations', min=min_val, max=max_val,
-                                        value=max_val / 2))
-
-        dist_plot = (ui.row(ui.column(5, ui.input_action_button('plot_distribution', 'Plot Histogram')),
-                            ui.column(7, ui.input_checkbox('enbl_plot', 'Other Plots'))),
-                     ui.panel_conditional('input.enbl_plot', ui.input_checkbox_group(
-                         "plot_props",
-                         "Other plots:", [],
-                     ), ui.input_action_button('plot_other', 'Plot Other')))
-
-        return (
-            ui.row(dist_head,
-                   dist_inputs,
-                   ),
-            dist_options,
-            dist_plot
-        )
 
 
 # TODO fix this MathJax. It works only once. On change of distribution the TypeSet is no
--- modules/distributions/ui.py
+++ modules/distributions/ui.py
@@ -1,19 +1,24 @@
-from typing import Literal
 
 from config import Config
-from shiny import module, ui
-import shiny.experimental as x
+from shiny import Inputs, Outputs, Session, module, render, ui, reactive
+from modules.common_ui import label_with_tooltip
 
 config = Config()
-cont_dist = config.input_config('distributions')['continuous']['names']
-discrete_dist = config.input_config('distributions')['discrete']['names']
+cont_dist = config.input_config('distributions')['continuous']
+discrete_dist = config.input_config('distributions')['discrete']
 qmark = config.ui_config('tooltip_q')
 
+config = Config()
+graph_height = config.ui_config('graph_height')
+dist_defaults = config.input_config('distributions')
+
+
+dist_names = cont_dist['names'] + discrete_dist['names']
 
 @module.ui
 def distribution_selection():
     return (
-        ui.input_radio_buttons('distributions', f'Distributions', sorted(cont_dist + discrete_dist)),
+        ui.input_radio_buttons('distributions', f'Distributions', sorted(dist_names)),
         # ui.output_ui('eq'),
         ui.hr(),
         ui.output_ui('inputs'),
@@ -21,20 +26,82 @@ def distribution_selection():
         ui.output_text_verbatim('details')
     )
 
+@module.server
+def create_dist_settings(input: Inputs, output: Outputs, session: Session):
+    @output
+    @render.ui
+    @reactive.event(input.distributions)
+    def inputs():
+        min_val = dist_defaults['min_obs']
+        max_val = dist_defaults['max_obs']
+        sd = dist_defaults['sd']
+        mean = dist_defaults['mean']
+        events = dist_defaults['events']
+        scale = dist_defaults['scale']
+        prob = dist_defaults['probability']
+        trials = dist_defaults['trials']
+        low = dist_defaults['low']
+        high = dist_defaults['high']
 
-def label_with_tooltip(label: str, q_mark: bool, tooltip_text: str,
-                       placement: Literal['right', 'left', 'top', 'down', 'auto'], tooltip_id: str):
-    if q_mark:
-        return x.ui.tooltip(
-            ui.span(label, qmark),
-            tooltip_text,
-            placement=placement,
-            id=tooltip_id,
+        properties_tooltip_text = 'Extra properties to Table & Plot'
+        seed_tooltip = 'By setting a seed the data set can be frozen'
+
+        dist_inputs = None
+
+        if input.distributions() == 'Normal':
+            dist_inputs = (ui.column(3, ui.input_numeric('mean', 'μ', value=mean)),
+                           ui.column(3, ui.input_numeric('sd', 'σ', value=sd)))
+
+        elif input.distributions() == 'Poisson':
+            dist_inputs = ui.column(4, ui.input_numeric('events', 'Events', value=events))
+
+        elif input.distributions() == 'Exponential':
+            dist_inputs = ui.column(4, ui.input_numeric('scale', 'Scale', value=scale))
+
+        elif input.distributions() == 'Geometric':
+            dist_inputs = ui.column(4, ui.input_numeric('prob', 'Probability', value=prob, step=0.05))
+
+        elif input.distributions() == 'Binomial':
+            dist_inputs = (ui.column(3, ui.input_numeric('trials', 'Trials', value=trials)),
+                           ui.column(3, ui.input_numeric('prob', 'Probability', value=prob, step=0.05)))
+
+        elif input.distributions() == 'Uniform':
+            dist_inputs = (ui.column(3, ui.input_numeric('low', 'Low', value=low)),
+                           ui.column(3, ui.input_numeric('high', 'High', value=high)))
+
+        # elif input.distributions() == 'Cauchy':
+        #     dist_inputs = (ui.column(3, ui.input_numeric('scale', 'Scale', value=scale)),
+        #                    ui.column(3, ui.input_numeric('location', 'Location', value=0))
+        #                    )
+
+        dist_head = (ui.column(3, ui.input_numeric('seed', label_with_tooltip('Seed ', True, seed_tooltip, 'right',
+                                                                              'seed_tooltip'), value=0)),
+                     ui.column(3, ui.input_numeric('max', 'Max Obs.', value=max_val)))
+
+        dist_options = (ui.input_selectize('prop',
+                                           label_with_tooltip('Properties ', True, properties_tooltip_text, 'right',
+                                                              'card_tooltip'),
+                                           discrete_dist['methods'], multiple=False),
+                        ui.row(ui.column(6, ui.input_checkbox('enbl_extra', 'Extra Properties'))),
+                        ui.panel_conditional('input.enbl_extra',
+                                             ui.input_selectize('extra_prop', 'Extra Properties',
+                                                                discrete_dist['extra_methods'],
+                                                                multiple=False)),
+                        ui.input_slider('observations', 'Observations', min=min_val, max=max_val,
+                                        value=max_val / 2))
+
+        dist_plot = (ui.row(ui.column(5, ui.input_action_button('plot_distribution', 'Plot Histogram')),
+                            ui.column(7, ui.input_checkbox('enbl_plot', 'Other Plots'))),
+                     ui.panel_conditional('input.enbl_plot', ui.input_checkbox_group(
+                         "plot_props",
+                         "Other plots:", [],
+                     ), ui.input_action_button('plot_other', 'Plot Other')))
+
+        return (
+            ui.row(dist_head,
+                   dist_inputs,
+                   ),
+            dist_options,
+            dist_plot
         )
-    else:
-        return x.ui.tooltip(
-            ui.span(label),
-            tooltip_text,
-            placement=placement,
-            id=tooltip_id,
-        )
+
